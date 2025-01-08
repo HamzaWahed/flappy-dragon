b1735a25867a52993d0fa23304e7a216d2c4b94c
hamza wahed <hzd454@gmail.com>
2025-01-08 12:26:54 -0400

    flesh out play function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 struct Player {
     x: i32,
     y: i32,
@@ -44,12 +48,16 @@ impl Player {
 
 struct State {
     mode: GameMode,
+    player: Player,
+    frame_time: f32,
 }
 
 impl State {
     fn new() -> Self {
         State {
             mode: GameMode::Menu,
+            player: Player::new(5, 25),
+            frame_time: 0.0,
         }
     }
 
@@ -69,7 +77,23 @@ impl State {
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::End;
+        // clear the context and make the background navy
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn dead(&mut self, ctx: &mut BTerm) {
@@ -89,6 +113,8 @@ impl State {
 
     fn restart(&mut self) {
         self.mode = GameMode::Playing;
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
     }
 }
 
