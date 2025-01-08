85f27c677785ed55770b32533d3832a8671fe5dd
hamza wahed <hzd454@gmail.com>
2025-01-08 13:19:33 -0400

    add obstacles and render them

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ const SCREEN_HEIGHT: i32 = 50;
 const FRAME_DURATION: f32 = 75.0;
 
 struct Player {
-    x: i32,
+    x: i32, // world position
     y: i32,
     velocity: f32, // downward velocity
 }
@@ -46,10 +46,50 @@ impl Player {
     }
 }
 
+struct Obstacle {
+    x: i32,     // world-space position
+    gap_y: i32, // center of the gap
+    size: i32,  // length of the gap
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),   // between 10 and 39
+            size: i32::max(2, 20 - score), // make walls smaller as player progresses
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let obstacle_screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        for y in 0..self.gap_y - half_size {
+            ctx.set(obstacle_screen_x, y, RED, BLACK, to_cp437('|'));
+        }
+
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(obstacle_screen_x, y, RED, YELLOW, to_cp437('|'));
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_below_gap || player_above_gap)
+    }
+}
+
 struct State {
     mode: GameMode,
     player: Player,
     frame_time: f32,
+    score: i32,
+    obstacle: Obstacle,
 }
 
 impl State {
@@ -58,6 +98,8 @@ impl State {
             mode: GameMode::Menu,
             player: Player::new(5, 25),
             frame_time: 0.0,
+            score: 0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
         }
     }
 
@@ -91,7 +133,18 @@ impl State {
 
         self.player.render(ctx);
         ctx.print(0, 0, "Press SPACE to flap.");
-        if self.player.y > SCREEN_HEIGHT {
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+
+        self.obstacle.render(ctx, self.player.x);
+
+        // passed the obstacle, create a new obstacle
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        // going upwards decreases height
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
